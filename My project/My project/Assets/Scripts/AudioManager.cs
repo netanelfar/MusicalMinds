74e5f7dbb0c45a2ea97f1d40161977162f20c46d
Netanel <netanel7358@gmail.com>
2025-04-02 13:08:10 +0300

    fixed git ignore + first real commit

---
+++ My project/My project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+    public AudioSource audioSource;
+    public AudioClip[] noteClips;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayNote(string clipName)
+    {
+        AudioClip clip = Resources.Load<AudioClip>("Notes/" + clipName);
+        if (clip == null)
+        {
+            Debug.LogError("Clip not found: " + clipName);
+            return;
+        }
+        if (audioSource == null)
+        {
+            Debug.LogError("AudioSource is not assigned.");
+            return;
+        }
+        audioSource.PlayOneShot(clip);
+    }
+}
